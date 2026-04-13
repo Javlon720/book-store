b51f6b6e5be7d28f3c8349750d766d8ae882badd
Javlon <abdullayevjavlon720@gmail.com>
2026-04-13 14:28:15 +0500

    chore: create config.js

--- shared/config/config.js
+++ shared/config/config.js
@@ -1,18 +1,21 @@
 import dotenv from "dotenv";
+import fs from "fs"
 import path from "path"
 import { fileURLToPath } from "url"
 
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const rootEnv = path.join(process.cwd(), ".env")
+const repoEnv = path.join(__dirname, "../../.env")
+const envPath = fs.existsSync(rootEnv) ? rootEnv : repoEnv
 
-dotenv.config({ path: path.join(__dirname,"../../.env") })
+dotenv.config({ path: envPath })
 
 export const config = {
-api_port:process.env.API_PORT,
-web_port:process.env.WEB_PORT,
-pg_host:process.env.PG_HOST,
-pg_port:process.env.PG_PORT,
-pg_user:process.env.PG_USER,
-pg_password:process.env.PG_PASSWORD,
-pg_database:process.env.PG_DATABASE,
+  api_port: process.env.API_PORT,
+  web_port: process.env.WEB_PORT,
+  pg_host: process.env.PG_HOST,
+  pg_port: process.env.PG_PORT,
+  pg_user: process.env.PG_USER,
+  pg_password: process.env.PG_PASSWORD,
+  pg_database: process.env.PG_DATABASE,
 }
